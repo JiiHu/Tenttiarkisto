abd0c140cbadd9d2cf50ee2f544cea9dd1e03294
Ville-Veikko Saari <ville-veikko.saari@cs.helsinki.fi>
2015-12-09 18:20:27 +0200

    fixes to author and added keyword param for courses

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -69,6 +69,6 @@ class ExamsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def exam_params
-      params.require(:exam).permit(:language, :date, :description)
+      params.require(:exam).permit(:language, :date, :description, :author)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151209153103) do
+ActiveRecord::Schema.define(version: 20151209160924) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20151209153103) do
     t.integer  "subject_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "keywords"
   end
 
   create_table "exams", force: :cascade do |t|
