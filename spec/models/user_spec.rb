05332a25373412372d934eace13e1ce0a001188f
Mapukto <maot@cs.helsinki.fi>
2015-12-12 04:33:07 +0200

    getting started with the tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "has the organization set correctly" do
+    user = User.new organization:"asd"
+
+    expect(user.organization).to eq("asd")
+  end
+
+  describe "with incorrect sign up info" do
+    it "is not saved without a password" do
+      user = User.create organization:"asd"
+
+      expect(user).not_to be_valid
+      expect(User.count).to eq(0)
+    end
+
+    it "is not saved with incorrect password_confirmation" do
+      user = User.create organization:"asd", password:"asd", password_confirmation:"asdd"
+
+      expect(user).not_to be_valid
+      expect(User.count).to eq(0)
+    end
+
+    it "is not saved with too short password" do
+      user = User.create organization:"asd", password:"asd", password_confirmation:"asd"
+
+      expect(user).not_to be_valid
+      expect(User.count).to eq(0)
+    end
+  end
+
+  describe "with proper sign up info" do
+    let(:user){ FactoryGirl.create(:user) }
+
+    it "is saved" do
+      expect(user).to be_valid
+      expect(User.count).to eq(1)
+    end
+
+
+  end
 end
